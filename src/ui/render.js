786f5599b918338553a45ced69679aa5bb9ac1a7
stenpten <1295402349@qq.com>
2026-05-13 14:28:21 +0800

    Fix memory edit HTML escaping

--- src/ui/render.js
+++ src/ui/render.js
@@ -474,6 +474,6 @@ function escapeHtml(value) {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/\"/g, "&quot;")
+    .replace(/"/g, "&quot;")
     .replace(/'/g, "&#039;");
 }
